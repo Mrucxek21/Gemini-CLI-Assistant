99b499de114b439f1650a2ff20268994fb2460a6
Mrucxek <ksaweryz1337@gmail.com>
2026-02-04 00:38:00 +0100

    Update check_models.py

--- check_models.py
+++ check_models.py
@@ -1,10 +1,9 @@
 from google import genai
 import os
 from dotenv import load_dotenv
-
+ 
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-# To wypisze listę WSZYSTKICH modeli dostępnych dla Twojego klucza
 for m in client.models.list(config={"page_size": 100}):
     print(m.name)
